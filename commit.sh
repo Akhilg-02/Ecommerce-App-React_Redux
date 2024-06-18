94dc0f2c595391b34671194ee02062dec1cb2018
unknown <guptaakhil0206@gmail.com>
2024-06-18 12:39:16 +0530

    added image

--- commit.sh
+++ commit.sh
@@ -8,7 +8,7 @@ git add --all
 git status
 
 # Commit changes with a specified message
-git commit -m "added route when click on application logo for home"
+git commit -m "added image"
 
 # Push changes to the remote repository
 git push origin main
